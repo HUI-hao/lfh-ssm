97756110b0185bb892a1b10896b3e6090a767c43
HUI-hao <lifeihao01@hotmail.com>
2023-03-10 16:29:37 +0800

    connmt vue

--- vue-ui/src/main.js
+++ vue-ui/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from "@/router";
+import router from "./router";
+import store from "./store";
 
 createApp(App).mount('#app')
-app.use(router).mount('#app')
+app.use(router).use(store).mount('#app')
