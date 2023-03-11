b740b40c3fb4a4b3957053c908bcd3516e282581
HUI-hao <lifeihao01@hotmail.com>
2023-03-11 20:54:49 +0800

    connmt spring

---
+++ ssm/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.lfh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
